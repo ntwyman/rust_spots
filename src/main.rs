37b2c7059e22b12430a94c892024f059b936ec7f
Nick Twyman <nick@conky.org>
2023-11-20 16:22:21 -0800

    Starting to convert to embedded display code

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@
 #![no_std]
 #![no_main]
 
+use gc9a01::display::DisplayResolution240x240;
+use gc9a01::mode::BufferedGraphics;
+use gc9a01::rotation::DisplayRotation;
+use gc9a01::Gc9a01;
+use gc9a01::SPIDisplayInterface;
 // The macro for our start-up function
 use seeeduino_xiao_rp2040::entry;
 
@@ -57,82 +62,12 @@ fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
-    let clocks = hal::clocks::init_clocks_and_plls(
-        seeeduino_xiao_rp2040::XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
-
-    // Set the pins up according to their function on this particular board
-    let pins = seeeduino_xiao_rp2040::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
+    let interface = SPIDisplayInterface::new(spi, dc, cs);
+    let mut display_driver = Gc9a01::new(
+        interface,
+        DisplayResolution240x240,
+        DisplayRotation::Rotate0,
     );
-
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    // Init PWMs
-    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
-
-    // Configure PWM4
-    let pwm = &mut pwm_slices.pwm0;
-    pwm.set_ph_correct();
-    pwm.enable();
-
-    // Output channel B on PWM0 to the red LED pin, initially off
-    let channel = &mut pwm.channel_b;
-    channel.output_to(pins.led_red);
-    channel.set_duty(u16::MAX);
-
-    // Set the blue LED to be an output, initially off
-    let mut led_blue_pin = pins.led_blue.into_push_pull_output_in_state(PinState::High);
-
-    // Turn off the green LED
-    let mut _led_green_pin = pins
-        .led_green
-        .into_push_pull_output_in_state(PinState::High);
-
-    loop {
-        info!("BLue");
-        // Blink blue LED at 1 Hz
-        for _ in 0..5 {
-            led_blue_pin.set_low().unwrap();
-            delay.delay_ms(500);
-            led_blue_pin.set_high().unwrap();
-            delay.delay_ms(500);
-        }
-
-        // Ramp red LED brightness up
-        for i in (LOW..=HIGH).skip(30) {
-            delay.delay_us(100);
-            channel.set_duty(u16::MAX - i);
-        }
-
-        // Ramp red LED brightness down
-        for i in (LOW..=HIGH).rev().skip(30) {
-            delay.delay_us(100);
-            channel.set_duty(u16::MAX - i);
-        }
-    }
 }
 
 // End of file
