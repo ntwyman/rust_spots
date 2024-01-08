7bce2a295e46c53d5c86d4dbe0c970e5b45358fd
Nick Twyman <nick@conky.org>
2024-01-08 14:29:08 -0800

    Drawing an image from the SD card - eye, eye

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,7 @@
 #![no_std]
 #![no_main]
 
-use embedded_graphics::{
-    pixelcolor::Rgb565,
-    prelude::{Point, RgbColor, Size},
-    primitives::{Circle, Primitive, PrimitiveStyleBuilder, Rectangle},
-    Drawable,
-};
+use embedded_graphics::{image::Image, prelude::Point, Drawable};
 
 use gc9a01::{
     display::DisplayResolution240x240, mode::DisplayConfiguration, rotation::DisplayRotation,
@@ -47,56 +42,16 @@ use hal::clocks::Clock;
 use defmt::*;
 use defmt_rtt as _;
 
-/// Test Function : will be removed later
-/*
-fn draw<I: WriteOnlyDataCommand, D: DisplayDefinition>(
-    display: &mut Gc9a01<I, D, BufferedGraphics<D>>,
-    tick: u32,
-) {
-    let (w, h) = display.dimensions();
-    let w = w as u32;
-    let h = h as u32;
-    let x = tick % w;
-    let y = tick % h;
+use embedded_sdmmc::{Mode, SdCard, TimeSource, Timestamp, VolumeIdx, VolumeManager};
+use tinyqoi::Qoi;
 
-    let style = PrimitiveStyleBuilder::new()
-        .stroke_width(4)
-        .stroke_color(Rgb565::new(tick as u8, x as u8, y as u8))
-        .fill_color(Rgb565::RED)
-        .build();
+mod qoiwrapper;
 
-    let cdiameter = 20;
-
-    // circle
-    Circle::new(
-        Point::new(119 - cdiameter / 2 + 40, 119 - cdiameter / 2 + 40),
-        cdiameter as u32,
-    )
-    .into_styled(style)
-    .draw(display)
-    .unwrap();
-
-    // circle
-    Circle::new(
-        Point::new(119 - cdiameter / 2 - 40, 119 - cdiameter / 2 + 40),
-        cdiameter as u32,
-    )
-    .into_styled(style)
-    .draw(display)
-    .unwrap();
-
-    // rectangle
-    let rw = 80;
-    let rh = 20;
-    Rectangle::new(
-        Point::new(119 - rw / 2, 119 - rh / 2 - 40),
-        Size::new(rw as u32, rh as u32),
-    )
-    .into_styled(style)
-    .draw(display)
-    .unwrap();
+fn panic_loop(err: &str) -> ! {
+    error!("PANIC --- {}", err);
+    loop {}
 }
-*/
+
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
@@ -137,96 +92,105 @@ fn main() -> ! {
     info!("Got clocks");
     let mut timer = hal::timer::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
+    // Set up the SPI bus
     let spi_mosi = pins.mosi.into_function::<hal::gpio::FunctionSpi>();
     let spi_miso = pins.miso.into_function::<hal::gpio::FunctionSpi>();
     let spi_sclk = pins.sck.into_function::<hal::gpio::FunctionSpi>();
-
     let spi: Spi<hal::spi::Disabled, pac::SPI0, (_, _, _), 8u8> =
         Spi::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
-    // Exchange the uninitialised SPI driver for an initialised one
     let spi = spi.init(
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
-        1.MHz(),
+        16.MHz(),
         embedded_hal::spi::MODE_0,
     );
 
+    // Bus manager so we can share the bus across devices
+    let bus = shared_bus::BusManagerSimple::new(spi);
+
     let dc = pins.a3.into_function::<FunctionSioOutput>();
     let cs = pins.a1.into_function::<FunctionSioOutput>();
-    let interface = SPIDisplayInterface::new(spi, dc, cs);
-    /*
-    let data = [0u8, 1u8, 0x0fu8, 0xaa, 0x55u8, 0x80u8, 0xffu8];
-
-    loop {
-        let xx: DataFormat = DataFormat::U8(&data);
-        let err = interface.send_commands(xx);
-        match err {
-            Ok(_) => debug!("OK"),
-            Err(_) => debug!("Error"),
-        }
-    }
-    */
+    let interface = SPIDisplayInterface::new(bus.acquire_spi(), dc, cs);
     let mut display_driver = Gc9a01::new(
         interface,
         DisplayResolution240x240,
         DisplayRotation::Rotate0,
     )
     .into_buffered_graphics();
-
-    // let mut reset = pins.sda.into_function::<FunctionSioOutput>();
-    //    display_driver.reset(&mut reset, &mut timer).ok();
     display_driver.init(&mut timer).ok();
     defmt::debug!("Driver configured!");
 
-    let mut tick: u32 = 0;
-    loop {
-        // debug!("Tick {}", tick);
-        display_driver.clear();
-        let (w, h) = display_driver.dimensions();
-        let w = w as u32;
-        let h = h as u32;
-        let x = tick % w;
-        let y = tick % h;
+    defmt::debug!("Connecting to SdCard");
+    let sd_cs = pins.a2.into_function::<FunctionSioOutput>();
+    let card = SdCard::new(bus.acquire_spi(), sd_cs, timer);
+    /// A dummy timesource, which is mostly important for creating files.
+    #[derive(Default)]
+    pub struct DummyTimesource();
 
-        let style = PrimitiveStyleBuilder::new()
-            .stroke_width(4)
-            .stroke_color(Rgb565::new(tick as u8, x as u8, y as u8))
-            .fill_color(Rgb565::RED)
-            .build();
-
-        let cdiameter = 20;
-
-        // circle
-        Circle::new(
-            Point::new(119 - cdiameter / 2 + 40, 119 - cdiameter / 2 + 40),
-            cdiameter as u32,
-        )
-        .into_styled(style)
-        .draw(&mut display_driver)
-        .unwrap();
-
-        // circle
-        Circle::new(
-            Point::new(119 - cdiameter / 2 - 40, 119 - cdiameter / 2 + 40),
-            cdiameter as u32,
-        )
-        .into_styled(style)
-        .draw(&mut display_driver)
-        .unwrap();
-
-        // rectangle
-        let rw = 80;
-        let rh = 20;
-        Rectangle::new(
-            Point::new(119 - rw / 2, 119 - rh / 2 - 40),
-            Size::new(rw as u32, rh as u32),
-        )
-        .into_styled(style)
-        .draw(&mut display_driver)
-        .unwrap();
-        display_driver.flush().unwrap();
-        tick += 1;
+    impl TimeSource for DummyTimesource {
+        // In theory you could use the RTC of the rp2040 here, if you had
+        // any external time synchronizing device.
+        fn get_timestamp(&self) -> Timestamp {
+            Timestamp {
+                year_since_1970: 0,
+                zero_indexed_month: 0,
+                zero_indexed_day: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+            }
+        }
     }
+    let mut volume_mgr = VolumeManager::new(card, DummyTimesource::default());
+    defmt::debug!("Volume managers created");
+
+    let volume = match volume_mgr.open_volume(VolumeIdx(0)) {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Error opening root volume: {}", defmt::Debug2Format(&e));
+            panic_loop("Open volume");
+        }
+    };
+
+    let dir = match volume_mgr.open_root_dir(volume) {
+        Ok(dir) => dir,
+        Err(e) => {
+            error!("Error opening root dir: {}", defmt::Debug2Format(&e));
+            panic_loop("Open root dir");
+        }
+    };
+
+    debug!("Root directory opened");
+    let file = match volume_mgr.open_file_in_dir(dir, "eye.qoi", Mode::ReadOnly) {
+        Ok(file) => file,
+        Err(e) => {
+            error!("Error opening image file: {}", defmt::Debug2Format(&e));
+            panic_loop("Open image file");
+        }
+    };
+    let mut image_buf = [0u8; 128 * 1024];
+    let read_count = volume_mgr.read(file, &mut image_buf).unwrap();
+    volume_mgr.close_file(file).unwrap();
+    debug!("Read {} bytes from eye.qoi", read_count);
+
+    debug!("Image data length {}", image_buf.len());
+
+    let qoi = match Qoi::new(&image_buf[..read_count]) {
+        Ok(qoi) => qoi,
+        Err(e) => {
+            error!("Error processing qoi file: {}", defmt::Debug2Format(&e));
+            panic_loop("Qoi format");
+        }
+    };
+    let wrapper = qoiwrapper::Wrapper { image: &qoi };
+    display_driver.clear();
+    Image::new(&wrapper, Point::zero())
+        .draw(&mut display_driver)
+        .unwrap();
+    display_driver.flush().unwrap();
+    debug!("Drawn image");
+
+    loop {}
 }
 
 // End of file
---
+++ src/qoiwrapper.rs
@@ -0,0 +1,42 @@
+use embedded_graphics::{
+    draw_target::{DrawTarget, DrawTargetExt},
+    geometry::{Dimensions, OriginDimensions, Size},
+    image::ImageDrawable,
+    pixelcolor::Rgb565,
+};
+use tinyqoi::Qoi;
+
+pub struct Wrapper<'a> {
+    pub image: &'a Qoi<'a>,
+}
+
+impl OriginDimensions for Wrapper<'_> {
+    fn size(&self) -> Size {
+        self.image.size()
+    }
+}
+
+impl ImageDrawable for Wrapper<'_> {
+    type Color = Rgb565;
+
+    fn draw<D>(&self, target: &mut D) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = Self::Color>,
+    {
+        target.fill_contiguous(
+            &self.image.bounding_box(),
+            self.image.pixels().map(|p| Rgb565::from(p)),
+        )
+    }
+
+    fn draw_sub_image<D>(
+        &self,
+        target: &mut D,
+        area: &embedded_graphics::primitives::Rectangle,
+    ) -> Result<(), D::Error>
+    where
+        D: DrawTarget<Color = Self::Color>,
+    {
+        self.draw(&mut target.translated(-area.top_left).clipped(area))
+    }
+}
